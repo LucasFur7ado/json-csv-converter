66cecc42fb38fbc95d821b45074b5134eecde4db
Lucas <luc4sfur7ado@gmail.com>
2023-02-06 23:47:16 -0300

    Array and single object can be converted

--- json.js
+++ json.js
@@ -2,6 +2,12 @@ import fs from "fs"
 
 const path = "./json-to-convert"
 const convertedPath = "./csvs"
+const getColumnsString = (cols) => {
+    let stringCols = ""
+    cols.map(c => stringCols += c.trim() + ",")
+    stringCols = stringCols.slice(0, -1) + "\n"
+    return stringCols
+}
 
 fs.readdir(path, (err, files) => {
     if (err) {
@@ -11,25 +17,27 @@ fs.readdir(path, (err, files) => {
     files.map(f => {
         fs.readFile(path + "/" + f, "utf-8", (error, data) => {
             const obj = JSON.parse(data)
-            if (obj.length) {
-                let newUrl = convertedPath + "/" + f.replace(".json", ".csv"),
-                    entries = [], stringCols = ""
+            if (obj.length || typeof obj == 'object') {
+                let newUrl = convertedPath + "/" + f.replace(".json", ".csv")
                 if (error) {
                     console.log("Algo malió sal => ", error)
                     return false
                 }
-                const columns = Object.keys(obj[0])
-                columns.map(c => stringCols += c.trim() + ",")
-                stringCols = stringCols.slice(0, -1) + "\n"
-                fs.writeFile(newUrl, stringCols, () => { return })
-                obj.map(o => {
-                    let stringRow = ""
-                    columns.map(c => {
-                        stringRow += o[c].trim() + ","
+                const columns = Object.keys(obj.length ? obj[0] : obj)
+                fs.writeFile(newUrl, getColumnsString(columns), () => { return })
+                if (obj.length)
+                    obj.map(o => {
+                        let stringRow = ""
+                        columns.map(c => stringRow += o[c] + ",")
+                        stringRow = stringRow.slice(0, -1) + "\n"
+                        fs.appendFile(newUrl, stringRow, () => { return })
                     })
+                else {
+                    let stringRow = ""
+                    columns.map(c => stringRow += (obj[c] + ","))
                     stringRow = stringRow.slice(0, -1) + "\n"
                     fs.appendFile(newUrl, stringRow, () => { return })
-                })
+                }
                 console.log("¡Convertido exitosamente!")
             }
         })
--- readme.md
+++ readme.md
@@ -5,7 +5,8 @@
 
 ## Requisitos: tener NodeJS instalado
 
-Para convertir un archivo JSON a CSV coloquelo en la carpeta **json-to-convert**.
+Para convertir un archivo JSON a CSV coloquelo en la carpeta **json-to-convert**,
+puede contener tanto un Array como un simple objeto.
 A continuación ejecute, posicionado en la carpeta raíz (**csv-converter**), el 
 comando **node json.js**.
 Para realizar el mismo proceso convirtiendo CSV a JSON use la carpeta **csv-to-convert**
@@ -18,8 +19,9 @@ los futuros campos del archivo JSON.
 
 ## Requirements: NodeJS installed
 
-To convert a JSON file to CSV put it on **json-to-convert** directory and 
-execute the following command: **node json.js**, being in the root directory
+To convert a JSON file to CSV put it on **json-to-convert** directory 
+The JSON can contain an Array or just an object.   
+And execute the following command: **node json.js**, being in the root directory
 (**csv-converter**).
 To do the same process but converting CSV to JSON use the **csv-to-convert** 
 directory and the following command: **node csv.js**.
